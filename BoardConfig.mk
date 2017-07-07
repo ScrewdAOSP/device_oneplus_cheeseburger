8fd3607d342e87d456841bc59870f07b22626943
Simao Gomes Viana <xdevs23@outlook.com>
2017-07-07 14:58:11 +0200

    cheeseburger: SafetyNet + add the usbcontroller
    Change-Id: I67426f727bda7cf67958e9afea97a9187710a6a3
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,13 +64,26 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_USES_QCOM_BSP := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=2048
+
+BOARD_KERNEL_CMDLINE := \
+        androidboot.hardware=qcom \
+        ehci-hcd.park=3 \
+        lpm_levels.sleep_disabled=1 \
+        sched_enable_hmp=1 \
+        sched_enable_power_aware=1 \
+        service_locator.enable=1 \
+        swiotlb=2048 \
+    androidboot.usbcontroller=a800000.dwc3 \
+    androidboot.selinux=permissive \
+    androidboot.verifiedbootstate=green \
+    androidboot.veritymode=enforcing
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_APPEND_DTB := true
+TARGET_KERNEL_BUILD_VARIANT := user
 
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
