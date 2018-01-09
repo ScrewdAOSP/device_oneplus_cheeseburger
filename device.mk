f10f9be26fc6468bf2ad44e2aee6c18bc3d48c84
Corey Edwards <ensabahnur16@gmail.com>
2018-01-09 15:27:11 -0600

    Revert "op5: add hw keys overlay"
    This reverts commit 2feca3b4d165d377eb49e78a95ee2b1f8fe40e0b.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- device.mk
+++ device.mk
@@ -24,9 +24,6 @@
 
 $(call inherit-product, vendor/oneplus/cheeseburger/cheeseburger-vendor.mk)
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
